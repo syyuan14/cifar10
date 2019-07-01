3df4941b37582fedb1d33640f8d33082c64bff1a
yuanshaoyi <740161499@qq.com>
2019-07-01 20:32:15 +0800

    add

--- net.py
+++ net.py
@@ -52,7 +52,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*5*5, 120)
+        self.conv3 = nn.Conv2d(16,32,5)
+        self.fc1 = nn.Linear(32*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
