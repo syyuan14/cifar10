e3b3ecbd7ad2c614b1d024d978686c3c3408e8ed
yuanshaoyi <740161499@qq.com>
2019-07-01 20:54:12 +0800

    asa

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from net import Net
 import torch as t
 import torchvision as tv
 import torch.nn as nn
@@ -6,11 +7,15 @@ import torchvision.transforms as transforms
 from torchvision.transforms import ToPILImage
 from torch import optim
 show = ToPILImage()
-from net import Net
 
-#加载测试模型
+# 加载测试模型
 net = Net()
 net.load_state_dict(t.load("net.pth"))
+# 定义数据的预处理
+transform = transforms.Compose([
+    transforms.ToTensor(),  # 转化为Tensor
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # 归一化
+])
 # 测试集
 testset = tv.datasets.CIFAR10(
     root='./data',
